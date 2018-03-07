cfa2ea03704803b90f2dcaeeb91df1137a4124e4
Ralph Etienne <ralphyetienne@yahoo.com>
2018-03-07 16:31:02 -0500

    configed/routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
    root 'welcome#index'
 
-   resources :doc
+   resources :docs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
