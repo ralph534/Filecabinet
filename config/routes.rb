812212f0bb064fd86f2f05219f73b0b587f3f6d0
Ralph Etienne <ralphyetienne@yahoo.com>
2018-03-10 12:55:36 -0500

    Authenticated end

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
    resources :docs
    authenticated :user do
      root 'doc#index', as: 'authenticated_root'
-
+   end
    root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
