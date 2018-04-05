c31891f96d6399da09c8c1ecd58ef0f3cd92ca85
Ralph Etienne <ralphyetienne@yahoo.com>
2018-04-05 16:02:27 -0400

    created views and css

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
    resources :docs
    authenticated :user do
-     root "doc#index", as: "authenticated_root"
+     root "docs#index", as: "authenticated_root"
    end
    root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
